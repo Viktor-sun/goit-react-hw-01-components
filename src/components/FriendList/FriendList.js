74d74511461b8f0ee2ae0650af031b703ddc020c
Viktor-sun <bobick.zdoh@gmail.com>
2021-02-15 19:27:32 +0200

    add styled-components

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 import styles from './FriendList.module.css';
+
+const IconIsOnline = styled.span`
+  display: block;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 20px;
+  background-color: ${props => (props.isOnline ? 'green' : 'brown')};
+`;
 
 const FriendListItem = ({ friends }) =>
   friends.map(friend => (
     <li className={styles.item} key={friend.id}>
-      <span className={friend.isOnline ? styles.online : styles.offline}></span>
+      {/*classic variant */}
+      {/* <span className={friend.isOnline ? styles.online : styles.offline}></span> {} */}
+      {/*styled-components variant */}
+      <IconIsOnline isOnline={friend.isOnline} />
       <img
         className="avatar"
         src={friend.avatar}
