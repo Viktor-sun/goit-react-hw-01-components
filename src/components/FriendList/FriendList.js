ef066fe43fb2f086b4f21c31e610198e1c4b0d46
Viktor-sun <bobick.zdoh@gmail.com>
2021-02-17 16:32:58 +0200

    ends frend list component

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,37 +1,28 @@
 import React from 'react';
-import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
 import styles from './FriendList.module.css';
-
-const IconIsOnline = styled.span`
-  display: block;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  margin-right: 20px;
-  background-color: ${props => (props.isOnline ? 'green' : 'brown')};
-`;
-
-const FriendListItem = ({ friends }) =>
-  friends.map(friend => (
-    <li className={styles.item} key={friend.id}>
-      {/*classic variant */}
-      {/* <span className={friend.isOnline ? styles.online : styles.offline}></span> */}
-      {/*styled-components variant */}
-      <IconIsOnline isOnline={friend.isOnline} />
-      <img
-        className="avatar"
-        src={friend.avatar}
-        alt={friend.name}
-        width="48"
-      />
-      <p className={styles.name}>{friend.name}</p>
-    </li>
-  ));
 
 const FriendList = ({ friends }) => (
   <ul className={styles.friendList}>
-    <FriendListItem friends={friends} />
+    {friends.map(friend => (
+      <li className={styles.item} key={friend.id}>
+        <FriendListItem
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      </li>
+    ))}
   </ul>
 );
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default FriendList;
---
+++ src/components/FriendList/FriendListItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import styles from './FriendList.module.css';
+import defaultIcon from './FriendList.svg';
+
+const IconIsOnline = styled.span`
+  display: block;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  margin-right: 20px;
+  background-color: ${props => (props.isOnline ? 'green' : 'brown')};
+`;
+
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <>
+    {/*classic variant */}
+    {/* <span className={isOnline ? styles.online : styles.offline}></span> */}
+    {/*styled-components variant */}
+    <IconIsOnline isOnline={isOnline} />
+    <img className="avatar" src={avatar} alt={name} width="48" />
+    <p className={styles.name}>{name}</p>
+  </>
+);
+
+FriendListItem.defaultProps = {
+  avatar: defaultIcon,
+  isOnline: false,
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool,
+};
+
+export default FriendListItem;
